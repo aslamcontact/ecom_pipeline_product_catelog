70be95a3a449329e497f314c0ac13210dd8f9fa8
aslamcontact <aslam.mycontact@gmail.com>
2023-09-16 15:21:11 +0530

    add clone project

--- jenkins/jenkinsFile.groovy
+++ jenkins/jenkinsFile.groovy
@@ -7,6 +7,15 @@ pipeline
                             ' -v /usr/bin/docker:/usr/bin/docker '
                 }
             }
+            environment{
+                volume='artifact'
+                gitImage='aslamimages/alpine-git:2'
+                buildImage="aslamimages/mvn_jdk_git:latest"
+                gitProjectUrl="https://github.com/aslamcontact/ecom_product_catelog.git"
+                deployImage="aslamimages/basic_api"
+
+            }
+
 
 
             options {
@@ -17,7 +26,12 @@ pipeline
                 stage('cloning')
                         {
                             steps {
-                                sh "echo aslam"
+                                sh  "docker volume create ${volume}"
+
+                                sh  "docker run --rm  --name test2 "+
+                                        "-v ${volume}:/app "+
+                                        "-w /app  ${gitImage} "+
+                                        "git clone ${gitProjectUrl}"
 
                             }
                         }
