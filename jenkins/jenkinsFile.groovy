12cfdc849a3e8171bbc10cea6db5882819ef7444
aslamcontact <aslam.mycontact@gmail.com>
2023-09-16 15:46:42 +0530

    add docker compose

--- jenkins/jenkinsFile.groovy
+++ jenkins/jenkinsFile.groovy
@@ -37,15 +37,15 @@ pipeline
                                         "-v ${volume}:/app "+
                                         "-v /var/run/docker.sock:/var/run/docker.sock "+
                                         "-v /usr/bin/docker:/usr/bin/docker "+
-                                        "-v /usr/bin/docker:/usr/bin/compose "+
+                                        "-v /usr/bin/compose:/usr/bin/compose "+
                                         "-w /app/ecom_product_catelog  ubuntu:latest "+
-                                        "docker compose up "
+                                        "docker compose up -d"
 
                                 sh  "docker run --rm  --name test4 "+
                                         "-v ${volume}:/app "+
                                         "-v /var/run/docker.sock:/var/run/docker.sock "+
                                         "-v /usr/bin/docker:/usr/bin/docker "+
-                                        "-v /usr/bin/docker:/usr/bin/compose "+
+                                        "-v /usr/bin/compose:/usr/bin/compose "+
                                         "-w /app/ecom_product_catelog  ubuntu:latest "+
                                         "docker compose down"
 
