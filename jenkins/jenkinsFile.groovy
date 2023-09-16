406197ac1b480f3b27f215fdc02944ab219bc5a5
aslamcontact <aslam.mycontact@gmail.com>
2023-09-16 15:01:28 +0530

    add test code

---
+++ jenkins/jenkinsFile.groovy
@@ -0,0 +1,25 @@
+pipeline
+        {
+            agent {
+                docker {
+                    image 'ubuntu:latest'
+                    args ' -v /var/run/docker.sock:/var/run/docker.sock ' +
+                            ' -v /usr/bin/docker:/usr/bin/docker '
+                }
+            }
+
+
+            options {
+                skipDefaultCheckout()
+            }
+            stages {
+
+                stage('cloning')
+                        {
+                            steps {
+                                sh "echo aslam"
+
+                            }
+                        }
+            }
+        }
